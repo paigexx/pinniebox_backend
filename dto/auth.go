83c2d7b9e52575d80b60dbbc004a1bea5bd21f9c
paigexx <pcjones12@gmail.com>
2024-11-20 14:57:20 -0600

    feat: returns chat ID

--- dto/auth.go
+++ dto/auth.go
@@ -6,6 +6,7 @@ import tgData "github.com/telegram-mini-apps/init-data-golang"
 type AuthResponse struct {
 	Ok      bool        `json:"ok"`
 	User    tgData.User `json:"user"`
+	ChatID  string		`json:"chat_id"`
 	Message string      `json:"message"`
 }
 
--- services/auth.go
+++ services/auth.go
@@ -2,13 +2,13 @@ package services
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/paigexx/telegram-go-server/dto"
-	initdata "github.com/telegram-mini-apps/init-data-golang"
 	tgData "github.com/telegram-mini-apps/init-data-golang"
 )
 
@@ -44,6 +44,7 @@ func (s *AuthService) Authenticate(c *gin.Context, initData *string, isMocked bo
         response := dto.AuthResponse{
             Ok:      true,
             User:    mockUserData,
+			ChatID:  "123456789",
             Message: "Using mocked data",
         }
         return response, nil
@@ -55,14 +56,14 @@ func (s *AuthService) Authenticate(c *gin.Context, initData *string, isMocked bo
 
 	if initData != nil {
 		// Validate the initData with the Telegram Bot Token and expiration time
-		err := initdata.Validate(*initData, telegramBotToken, expiration)
+		err := tgData.Validate(*initData, telegramBotToken, expiration)
 		if err != nil {
 			log.Println("Error validating initData:", err)
 			return dto.AuthResponse{}, errors.New("invalid initData")
 		}
 
 		// Parse the initData to get user data
-		initDataParsed, err := initdata.Parse(*initData)
+		initDataParsed, err := tgData.Parse(*initData)
 		if err != nil {
 			log.Println("Error parsing initData:", err)
 			return dto.AuthResponse{}, errors.New("failed to parse initData")
@@ -71,6 +72,7 @@ func (s *AuthService) Authenticate(c *gin.Context, initData *string, isMocked bo
 		response := dto.AuthResponse{
 			Ok:       true,
 			User:     initDataParsed.User,
+			ChatID:   fmt.Sprint(initDataParsed.ChatInstance),
 			Message:  "Using parsed data",
 		}
 		return response, nil
